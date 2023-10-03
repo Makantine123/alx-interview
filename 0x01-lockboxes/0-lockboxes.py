ec4cb2111dcab632f67b62768e9b55b13517eceb
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-10-03 19:41:35 +0200

    Commit

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Unlock boxes Modules"""
+
+
+def canUnlockAll(boxes):
+    """Function method that determines if all the boxes can be opened"""
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
