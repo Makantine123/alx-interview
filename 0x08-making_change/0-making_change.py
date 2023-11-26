50abcc39220b440d9246b8156ab5fa2be5cdbf0c
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-11-26 10:12:06 +0200

    Commit

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""makingChange module"""
+
+
+def makeChange(coins, total):
+    """Function"""
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    change = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        temp = total // coin
+        change += temp
+        total -= (temp * coin)
+    if total != 0:
+        return -1
+    return change
