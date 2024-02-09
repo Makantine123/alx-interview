5a7b77cb574b5ac3f262f0d45d47a0fe9a004cd3
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-02-09 13:24:11 +0200

    Commit

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -25,7 +25,7 @@ def island_perimeter(grid):
                     cellperimeter = cellperimeter - 1
                 if x + 1 < len(grid) and grid[x + 1][y] == 1:
                     cellperimeter = cellperimeter - 1
-                if grid[x][y - 1] and grid[x][y - 1] == 1:
+                if y - 1 > -1 and grid[x][y - 1] == 1:
                     cellperimeter = cellperimeter - 1
                 if grid[x][y + 1] and grid[x][y + 1] == 1:
                     cellperimeter = cellperimeter - 1
