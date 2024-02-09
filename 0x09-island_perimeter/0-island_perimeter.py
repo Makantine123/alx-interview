7a25844bd5db80188454046ec81188d4fae8741b
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-02-09 13:01:06 +0200

    Commit

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module contains island_perimeter function"""
+
+
+def island_perimeter(grid):
+    """
+    Functions returns the perimeter of the island described in grid
+    grid: list of integers
+        * 0 represents water
+        * 1 represents land
+        * each cell is a square, with a side length of 1
+        * cells are connected horizontally/vertically (not diagonally)
+        * grind is rectangular, with its width and height not exceeding 100
+    The grid is surrounded by water
+    """
+    perimeter = 0
+
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 0:
+                continue
+            if grid[x][y] == 1:
+                cellperimeter = 4
+                if grid[x - 1][y] and grid[x - 1][y] == 1:
+                    cellperimeter = cellperimeter - 1
+                if grid[x + 1][y] and grid[x + 1][y] == 1:
+                    cellperimeter = cellperimeter - 1
+                if grid[x][y - 1] and grid[x][y - 1] == 1:
+                    cellperimeter = cellperimeter - 1
+                if grid[x][y + 1] and grid[x][y + 1] == 1:
+                    cellperimeter = cellperimeter - 1
+                perimeter = perimeter + cellperimeter
+    return perimeter
