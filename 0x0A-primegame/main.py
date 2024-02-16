99fe453a7cbe0a00f5d64e91ceffbebd6151214a
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2024-02-16 23:40:23 +0200

    Update

--- 0x0A-primegame/main.py
+++ 0x0A-primegame/main.py
@@ -5,8 +5,5 @@ Main file for testing
 
 isWinner = __import__('0-prime_game').isWinner
 
-nums = [0] * 10000
-for i in range(10000):
-    nums[i] = i
 
-print("Winner: {}".format(isWinner(10000, nums)))
+print("Winner: {}".format(isWinner(5, [1, 2, 3, 4, 5])))
